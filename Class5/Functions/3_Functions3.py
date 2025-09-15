eceed033e55aa27ce41d3054d6479461356b5615
unknown <bsuraj23@gmail.com>
2025-09-15 19:26:59 +0530

    adding class 5 6 7

---
+++ Class5/Functions/3_Functions3.py
@@ -0,0 +1,23 @@
+#TODO
+#position argument 
+
+
+
+
+def check_even_odd(number=90):  #default argument
+   
+    a=90
+    if number % 2 == 0:
+        print(number, "is Even")
+    else:
+        print(number, "is Odd")
+
+check_even_odd()
+check_even_odd(7)
+
+
+
+
+
+
+
---
+++ Class5/Functions/5_Functions5.py
@@ -0,0 +1,17 @@
+#TODO
+#Dafult Argument 
+
+def repeat_line(a="default", b=5):
+    for i in range(b):
+        print(a)
+
+
+
+
+# repeat_line("ashok IT",5)
+
+repeat_line()
+# repeat_line(2,"sometext")
+
+
+
